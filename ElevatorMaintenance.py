69fb153c4795a0d5c29c6334b59bdc7b3f7b5815
Lilach Naor <55951495+Lilach26@users.noreply.github.com>
2020-12-08 22:38:44 +0200

    Delete ElevatorMaintenance.py

--- ElevatorMaintenance.py
+++
@@ -1,7 +0,0 @@
-def solution(l):
-    split_list = [i.split(".") for i in l]
-    list_to_sort = [map(int, i) for i in split_list]
-    sorted_list = sorted(list_to_sort)
-    final_list = [('.'.join(str(i) for i in j)) for j in sorted_list]
-
-    return final_list
